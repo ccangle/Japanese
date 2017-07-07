43cdd8c39eb97b59c1f169c059b85bcf53ff0117
ccangle <1042299196@qq.com>
2017-07-07 22:33:28 +0800

    ""

--- js/index.js
+++ js/index.js
@@ -22,6 +22,11 @@ $(document).ready(function(){
     });
 
     $('.basic_conversation_index').click(function(){
-        $(this).parent().find('.basic_conversation_content').toggle(800);
+        // $(this).parent().find('.basic_conversation_content').toggle(800);
+
+        //自定义动画
+        // $(this).parent().find('.basic_conversation_content').animate({height: 'toggle'}, 'slow');
+        // $(this).parent().find('.basic_conversation_content').animate({width: 'toggle'}, 'slow');
+        $(this).parent().find('.basic_conversation_content').animate({height: 'toggle', opacity: 'toggle'}, 'slow');
     });
 });
